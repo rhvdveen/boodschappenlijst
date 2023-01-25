80df08b106855363c3c14f8a7ea079bbe3d8203a
rhvdveen <123541241+rhvdveen@users.noreply.github.com>
2023-01-25 20:13:21 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,23 @@ from gsheetsdb import connect
 # Create a connection object
 conn = connect()
 
+""""
+grocery_list = []
+
+more_items = True
+
+while more_items:
+  grocery_item = input("Wat?")
+  grocery_list.append(grocery_item)
+  
+  keep_adding_items = (input("Meer?")).lower()
+  
+  if keep_adding_items != 'y':
+    more_items = False
+ 
+"""
+
+
 # Perform SQL query on the Google Sheet.
 # Uses st.cache to only return when the query changes or after 10 min.
 @st.cache(ttl=600)
@@ -17,9 +34,18 @@ def run_query(query):
 st.title('Boodschappenlijst 2.0')
 st.header('Huidige lijst')
 
-if st.button('Haal lijst op'):
+
+
+
+# if st.button('Haal lijst op'):
   sheet_url = st.secrets["public_gsheets_url"]
   rows = run_query(f'SELECT * FROM "{sheet_url}"')
-  st.dataframe(rows)
+ # st.dataframe(rows)
+col1, col2 = st.columns(2)
+for row in rows:
+  with col1:
+    st.write(f{row.winkel)
+  with col2:
+    st.write(f{row.product)
 
 st.write("Hello world!")
