ee127a22a40d37ad5cd23bc61eb51c0d61c247f1
rhvdveen <123541241+rhvdveen@users.noreply.github.com>
2023-01-25 19:03:58 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,8 +15,8 @@ def run_query(query):
   return rows
 
 sheet_url = st.secrets["public_gsheets_url"]
-rows = run_query(f'SELECT * FROM "{sheet_url}"')
+rows = run_query('SELECT * FROM "{sheet_url}"')
 
-st.dataframe(rows)
+# st.dataframe(rows)
 
 st.write("Hello world!")
