c546e441d4ec378b49738e4a45e39c69358dee67
rhvdveen <123541241+rhvdveen@users.noreply.github.com>
2023-01-25 19:13:56 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,7 @@ from gsheetsdb import connect
 # Create a connection object
 conn = connect()
 
+
 # Perform SQL query on the Google Sheet.
 # Uses st.cache to only return when the query changes or after 10 min.
 @st.cache(ttl=600)
@@ -13,6 +14,11 @@ def run_query(query):
   rows = conn.execute(query, headers=1)
   rows = rows.fetchall()
   return rows
+
+st.write("Hello world!")
+
+st.stop()
+
 
 sheet_url = st.secrets["public_gsheets_url"]
 rows = run_query('SELECT * FROM "{sheet_url}"')
